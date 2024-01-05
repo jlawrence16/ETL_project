f6fb481fd663e2eece9c1c96c11c1e9fa332d210
[jlawrence16] <[tamhlacht@gmail.com]>
2024-01-05 13:51:29 -0500

    database creation section complete, readme added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ETL_project
+
+For this project, I built an ETL pipeline using Python, Pandas, and either Python dictionary method to extract and transform data. After I transformed the data, I created four CSV files and used the CSV file data to create an ERD and a table schema. Finally, I used the schema to set up Postgres database and imported the data from the CSV files.
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,72 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/5UoCYq
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(255)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" VARCHAR(255)   NOT NULL,
+    "currency" VARCHAR(255)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
+	CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id")
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
+	CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id")
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+	CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id")
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+--Imported data from CSV files using pgAdmins
+
+-- campaigns csv was imported last
+
+-- The following queries were run to ensure data was imported
+
+SELECT * FROM campaign
+;
+SELECT * FROM category
+;
+SELECT * FROM subcategory
+;
+SELECT * FROM contacts
+;
+
